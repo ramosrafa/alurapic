be650190c6b790803d9bfd2da1f8352304145c32
Rafael Ramos Rocha <rocharafaelramos@gmail.com>
2022-05-05 11:04:38 -0300

    Teste de commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'alurapic';
+  photos = [
+    {
+      url:'https://static-cse.canva.com/blob/613334/freestockphotos.jpg',
+      description: 'Leão'
+    },
+    {
+      url:'https://i.pinimg.com/originals/1e/94/b1/1e94b117d29d1f5c5c272d9eda17d9dc.png',
+      description: 'Paisagem'
+    }
+  ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
+import { PhotosModule } from './photos/photos.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, PhotosModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
